fe969169ae8c7fce929dca621e7930f59541c3a1
Marco Antonio <159474548+Markyneos@users.noreply.github.com>
2024-11-18 23:16:25 -0300

    Delete 2B/exercícioAvaliativo/exercicio.py

--- 2B/exercícioAvaliativo/exercicio.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import datetime
-
-arquivoArray = np.loadtxt("vendas.csv", str, delimiter=",")
-quantidadeVendida = arquivoArray[1:,3].astype(int)
-precoU = arquivoArray[1:,4].astype(float)
-valorT = arquivoArray[1:,5].astype(float)
